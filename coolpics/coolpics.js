c8f0c85215cf34b7799992d626017fa6b5fdaafc
tam24006 <tam24006@byui.edu>
2025-05-14 12:23:09 -0600

    new work

---
+++ coolpics/coolpics.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+ btn.addEventListener('click', ToggleMenu);
+ function ToggleMenu(){
+    menu.classList.toggle('hide');
+
+ }
+ const gallery = document.querySelector('.gallery');
+ 
--- mission/mission.js
+++ mission/mission.js
@@ -1,2 +1,21 @@
 let selectElem = document.querySelector('select');
-let logo = document
+let logo_EM = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme(){
+    let current = selectElem.value;
+    if(current == "dark"){
+        //change body to dark
+        document.body.className="dark";
+
+        //change logo to new dark logo
+        logo_EM.setAttribute('src',"./image/byui-logo_dark.png")
+    }
+    else if (current == 'light'){
+        //change body from black to white
+        document.body.className='white';
+        //change logo to old logo
+        logo_EM.setAttribute('src','./images/byui-logo_blue.png')
+    }
+}
