0938970c8a06ca2785e668b879f2459d315cf41b
tam24006 <tam24006@byui.edu>
2025-05-21 11:41:43 -0600

    new changes

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,9 +1,44 @@
 let btn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
- btn.addEventListener('click', ToggleMenu);
- function ToggleMenu(){
-    menu.classList.toggle('hide');
+function ToggleMenu() {
+  menu.classList.toggle('hide');
 
- }
- const gallery = document.querySelector('.gallery');
- 
+}
+btn.addEventListener('click', ToggleMenu);
+
+
+const gallery = document.querySelector('.gallery');
+
+const modalIamge = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+gallery.addEventListener('click', (event) => {
+  const img = event.target.closest('img');
+  const src = clickedImage.getAttribute('src');
+  const alt = clickedImage.getAttribute('alt');
+  const full = src.split('-')[0] + '-full.jpeg';
+
+  modalImage.src = full;
+  modalImage.alt = alt;
+  modal.showModal();
+
+});
+
+
+modal.addEventListener('click', (event) => {
+  if (event.target.classList.contains('close-viewer')) {
+    modal.close();
+  }
+});
+
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+});
+
+
+
+
+
