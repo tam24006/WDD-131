86cd51341f5e038942c6233215be78980afd74ce
tam24006 <tam24006@byui.edu>
2025-05-28 11:37:35 -0600

    update

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,43 +1,100 @@
-let btn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-function ToggleMenu() {
-  menu.classList.toggle('hide');
+// let btn = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.menu');
+// function ToggleMenu() {
+//   menu.classList.toggle('hide');
 
+// }
+// btn.addEventListener('click', ToggleMenu);
+
+
+// const gallery = document.querySelector('.gallery');
+
+// const modalIamge = modal.querySelector('img');
+// const closeButton = modal.querySelector('.close-viewer');
+
+// gallery.addEventListener('click', (event) => {
+//   const img = event.target.closest('img');
+//   const src = clickedImage.getAttribute('src');
+//   const alt = clickedImage.getAttribute('alt');
+//   const full = src.split('-')[0] + '-full.jpeg';
+
+//   modalImage.src = full;
+//   modalImage.alt = alt;
+//   modal.showModal();
+
+// });
+
+
+// modal.addEventListener('click', (event) => {
+//   if (event.target.classList.contains('close-viewer')) {
+//     modal.close();
+//   }
+// });
+
+
+// modal.addEventListener('click', (event) => {
+//   if (event.target === modal) {
+//     modal.close();
+//   }
+// });
+
+
+
+
+
+// show and hide menu items 
+const menuButton = document.querySelector("#menu-button");
+
+function showMenu() {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("hide");
+    console.log("Menu toggled!");
 }
-btn.addEventListener('click', ToggleMenu);
+
+menuButton.addEventListener("click", showMenu);
+
+// Window resize
+function handleResize() {
+    const navLinks = document.querySelector(".nav-links");
+    if (window.innerWidth > 1000) {
+        navLinks.classList.remove("hide");  
+    } else {
+        navLinks.classList.add("hide");  
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+// image viewer
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img class="image" src="${pic}" alt="${alt}" />
+  </div>`
+}
+
+function closeViewer(event) {
+    document.querySelector(".viewer").remove()
+}
+
+function viewHandler(event) {
+    // create a variable to hold the element that was clicked on from event.target
+    const clickedImage = event.target;
+	// get the src attribute from that element and 'split' it on the "-"
+    const srcValue = clickedImage.getAttribute("src").split("-");
+	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+    const newFilename = srcValue[0] + "-full.jpeg";
+	// insert the viewerTemplate into the top of the body element
+	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
+    document.querySelector("body").insertAdjacentHTML("afterbegin", viewerTemplate(newFilename, "image viewer"));
+	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+}
 
 
-const gallery = document.querySelector('.gallery');
-
-const modalIamge = modal.querySelector('img');
-const closeButton = modal.querySelector('.close-viewer');
-
-gallery.addEventListener('click', (event) => {
-  const img = event.target.closest('img');
-  const src = clickedImage.getAttribute('src');
-  const alt = clickedImage.getAttribute('alt');
-  const full = src.split('-')[0] + '-full.jpeg';
-
-  modalImage.src = full;
-  modalImage.alt = alt;
-  modal.showModal();
-
-});
-
-
-modal.addEventListener('click', (event) => {
-  if (event.target.classList.contains('close-viewer')) {
-    modal.close();
-  }
-});
-
-
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.close();
-  }
-});
-
+document.querySelector(".gallery").addEventListener("click", viewHandler);
 
 
 
--- mission/mission.js
+++ mission/mission.js
@@ -1,21 +1,45 @@
-let selectElem = document.querySelector('select');
-let logo_EM = document.querySelector('img');
+// let selectElem = document.querySelector('select');
+// let logo_EM = document.querySelector('img');
 
-selectElem.addEventListener('change', changeTheme);
+// selectElem.addEventListener('change', changeTheme);
 
-function changeTheme(){
-    let current = selectElem.value;
-    if(current == "dark"){
-        //change body to dark
-        document.body.className="dark";
+// function changeTheme(){
+//     let current = selectElem.value;
+//     if(current == "dark"){
+//         //change body to dark
+//         document.body.className="dark";
 
-        //change logo to new dark logo
-        logo_EM.setAttribute('src',"./image/byui-logo_dark.png")
-    }
-    else if (current == 'light'){
-        //change body from black to white
-        document.body.className='white';
-        //change logo to old logo
-        logo_EM.setAttribute('src','./images/byui-logo_blue.png')
+//         //change logo to new dark logo
+//         logo_EM.setAttribute('src',"./image/byui-logo_dark.png")
+//     }
+//     else if (current == 'light'){
+//         //change body from black to white
+//         document.body.className='white';
+//         //change logo to old logo
+//         logo_EM.setAttribute('src','./images/byui-logo_blue.png')
+//     }
+// }
+
+
+
+const themeSelector = document.querySelector('#theme');
+
+// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
+
+function changeTheme() {
+// check to see what the current value of our select is.
+// The current value is conveniently found in themeSelector.value!
+    console.log(themeSelector.value);
+
+    if (themeSelector.value === 'dark') {
+        document.querySelector('body').classList.add('dark');
+        document.querySelector('img').src = 'byui-logo_white.png';
+    } else {
+        document.querySelector('body').classList.remove('dark');
+        document.querySelector('img').src = 'byui-logo_blue.webp';
     }
 }
+
+// add an event listener to the themeSelector element here.
+// Use the changeTheme function as the event handler function.
+themeSelector.addEventListener('change', changeTheme);
